30a604ee9da73cfe5835b7a2e5638049917abb4c
Alexandra Qin <alexandraqin@gmail.com>
2013-03-04 20:32:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happiness::Application.config.secret_token = 'efcea4336eb12ebbe02d02bd4283c8943a098befb476054e762dd5982a96c610ac77ae9c3fead9a80fca4547478db3b7a2140fd1752614ba3014a50a9f655b95'
