64a8ccc55b19965bd223e899b995e008c008f6ea
Alexandra Qin <alexandraqin@gmail.com>
2013-03-20 16:33:29 -0400

    pulls #thankful tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  
   def index
+    @tweets = []
+    Twitter.search("#thankful", count: 10).results.map do |result|
+      @tweets << result
+    end
   end
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "yQXwQmXwTTGRlad63U1qQ"
+  config.consumer_secret = "JEilUhzgZYOix2ZpvQJnjAvhoJA6wJCWeSrSgx0fc"
+  config.oauth_token = "201589912-KzJNbppbhsj1OflpVpEhUtUJEE8daVAjf2rcGeSc"
+  config.oauth_token_secret = "mzUkHdLW3NhEM3ekqeUMRnwny05AEbgFLpMbpXm6ZqU"
+end
