409309f11bd6e8dff76236940473b51ee4be9197
Alexandra Qin <alexandraqin@gmail.com>
2013-04-17 00:12:52 -0400

    stuffs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Happiness::Application.routes.draw do
   
   resources :users
 
-
   root :to => "home#index"
   match "/logout" => "sessions#destroy", :as => :logout
   match "/auth/:provider/callback" => "sessions#create"
